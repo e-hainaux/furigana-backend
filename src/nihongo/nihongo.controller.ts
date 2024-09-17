87dcac2769a843ae29a0fc19e77a0fdbd2fb1756
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-09-17 12:06:30 +0200

    feat nihongo/convert to furigana ok

--- src/nihongo/nihongo.controller.ts
+++ src/nihongo/nihongo.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { NihongoService } from './nihongo.service';
 
 @Controller('nihongo')
-export class NihongoController {}
+export class NihongoController {
+  constructor(private readonly nihongoService: NihongoService) {}
+
+  @Post('convert')
+  async convertToFurigana(
+    @Body() body: { text: string; to?: 'furigana' | 'hiragana' | 'romaji' },
+  ) {
+    const { text, to = 'furigana' } = body;
+    return await this.nihongoService.convertToFurigana(text, to);
+  }
+}
--- src/nihongo/nihongo.service.ts
+++ src/nihongo/nihongo.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import Kuroshiro from 'kuroshiro';
+import * as KuromojiAnalyzer from 'kuroshiro-analyzer-kuromoji';
 
 @Injectable()
-export class NihongoService {}
+export class NihongoService implements OnModuleInit {
+  private kuroshiro: Kuroshiro;
+
+  async onModuleInit() {
+    this.kuroshiro = new Kuroshiro();
+    await this.kuroshiro.init(new KuromojiAnalyzer());
+  }
+
+  async convertToFurigana(
+    text: string,
+    to: 'furigana' | 'hiragana' | 'romaji' = 'furigana',
+  ): Promise<string> {
+    try {
+      console.log('text : ', text);
+      const result = await this.kuroshiro.convert(text, {
+        to: to,
+        mode: 'furigana',
+      });
+      console.log('result : ', result);
+
+      return result;
+    } catch (error) {
+      console.error('Error converting text:', error);
+      throw new Error('Failed to convert text');
+    }
+  }
+}
