5e951b13e12e1cf5b2bb2bc1d0222a3903c1bc50
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-09-18 15:29:12 +0200

    fix vercel deployment files

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://furigana-app.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
